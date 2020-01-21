d27aef665ff1bb009fea26fc808948f436de0a5d
Albert Corsair <neata98@outlook.com>
2020-01-21 23:50:16 +0100

    Creada pantalla de lista
    Took 46 minutes

---
+++ app/src/main/java/com/neata/gastos/ItemsListActivity.java
@@ -0,0 +1,14 @@
+package com.neata.gastos;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ItemsListActivity  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_view);
+    }
+}
--- app/src/main/java/com/neata/gastos/MainActivity.java
+++ app/src/main/java/com/neata/gastos/MainActivity.java
@@ -1,14 +1,29 @@
 package com.neata.gastos;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CardView gastosCardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gastosCardView = findViewById(R.id.gastosCardView);
+        gastosCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ItemsListActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
