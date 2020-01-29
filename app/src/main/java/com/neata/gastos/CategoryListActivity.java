198676c60b2308ab34cddf4d5f0db58615d84a20
Albert Corsair <neata98@outlook.com>
2020-01-29 22:57:50 +0100

    Categorías completo
    La aplicación permite añadir y borrar categorías
    
    Took 49 minutes

--- app/src/main/java/com/neata/gastos/CategoryListActivity.java
+++ app/src/main/java/com/neata/gastos/CategoryListActivity.java
@@ -1,12 +1,15 @@
 package com.neata.gastos;
 
+import android.content.DialogInterface;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -23,6 +26,7 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.neata.gastos.modelo.Categoria;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -39,38 +43,143 @@ public class CategoryListActivity extends AppCompatActivity {
     private CategoryAdapter adapter;
 
     private List<Categoria> categorias;
+
+    private AlertDialog addCategoryDialog;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_view);
-
-        fillCategories();
-
+        setTitle("Categorías");
         recycler = findViewById(R.id.recyclerView);
         recycler.setHasFixedSize(true);
         recycler.setItemAnimator(new DefaultItemAnimator());
         mLayoutManager = new LinearLayoutManager(this);
         recycler.setLayoutManager(mLayoutManager);
 
+        categorias = new ArrayList<>();
+
+        adapter = new CategoryAdapter(categorias, R.layout.category_item, new CategoryAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(final Categoria categoria, final int position) {
+                //TODO: abrir dialog para borrar categoria
+                AlertDialog.Builder builder = new AlertDialog.Builder(CategoryListActivity.this);
+// Add the buttons
+                builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        db.collection("categoria").document(categoria.getId())
+                                .delete()
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        categorias.remove(position);
+                                        recycler.removeViewAt(position);
+                                        adapter.notifyItemRemoved(position);
+                                        adapter.notifyItemRangeChanged(position, categorias.size());
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        throw new Resources.NotFoundException("No se ha podido borrar la categoría");
+                                    }
+                                });
+                    }
+                });
+                builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+                // Set other dialog properties
+                builder.setMessage("¿Quieres borrar esta categoría?")
+                        .setTitle("Borrar categoría");
+
+
+                // Create the AlertDialog
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+
+            }
+
+        });
+
+        recycler.setAdapter(adapter);
         fab = findViewById(R.id.FABAddCity);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO: gestionar click en el fab con un dialog para añadir categoria
+
+                addCategoryDialog.show();
             }
         });
         fab = findViewById(R.id.FABAddCity);
 
         progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
 
+        fillCategories();
+        createDialog();
+
+    }
+
+    private void createDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(CategoryListActivity.this);
+        final EditText input = new EditText(CategoryListActivity.this);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        // Add the buttons
+        builder.setPositiveButton("Guardar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked OK button
+                final String editText = input.getText().toString();
+                Map<String, Object> categoria = new HashMap<>();
+                categoria.put("nombre", editText);
+
+                db.collection("categoria")
+                .add(categoria)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        //Log.d("Good", "DocumentSnapshot added with ID: " + documentReference.getId());
+                        categorias.add(new Categoria(documentReference.getId(), editText));
+                        adapter.notifyDataSetChanged();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        try {
+                            throw new IOException("No se ha podido guardar la categoría");
+                        } catch (IOException ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
+            }
+        });
+        // Set other dialog properties
+        builder.setMessage("¿Quieres añadir una nueva categoría?")
+                .setTitle("Añadir nueva categoría");
+
+        input.setLayoutParams(lp);
+        builder.setView(input);
+
+        // Create the AlertDialog
+       addCategoryDialog = builder.create();
 
     }
 
     private void fillCategories(){
-        categorias = new ArrayList<>();
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-
         db.collection("categoria")
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
@@ -79,10 +188,7 @@ public class CategoryListActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
 
                             for (QueryDocumentSnapshot document : task.getResult()) {
-                                System.out.println("good: " + document.getId() + " => " + document.getData());
-
                                 categorias.add(new Categoria(document.getId(), (String) document.getData().get("nombre")));
-                                System.out.println(document.getData().get("nombre"));
                             }
                             showResults();
                         } else {
@@ -91,37 +197,11 @@ public class CategoryListActivity extends AppCompatActivity {
                     }
                 });
 
-//                Map<String, Object> user = new HashMap<>();
-//                user.put("nombre", "almuerzo");
-//
-//                db.collection("categoria")
-//                .add(user)
-//                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-//                    @Override
-//                    public void onSuccess(DocumentReference documentReference) {
-//                        System.out.println("HEHEHHE: "+ documentReference.getId());
-//                        //Log.d("Good", "DocumentSnapshot added with ID: " + documentReference.getId());
-//                    }
-//                })
-//                .addOnFailureListener(new OnFailureListener() {
-//                    @Override
-//                    public void onFailure(@NonNull Exception e) {
-//                        Log.w("Bad", "Error adding document", e);
-//                    }
-//                });
     }
 
     private void showResults() {
 
-        adapter = new CategoryAdapter(categorias, R.layout.category_item, new CategoryAdapter.OnItemClickListener() {
-            @Override
-            public void onItemClick(Categoria categoria, int position) {
-                //TODO: abrir dialog para borrar categoria
-            }
-
-        });
-
-        recycler.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
         progressBar.setVisibility(View.GONE);
     }
 }
